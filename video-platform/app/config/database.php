ee64cc611fccfe688c84adb473df3acc2f6bf9ea
RikJongejan <6044223@mborijnland.nl>
2026-05-06 14:13:20 +0200

    database connectie

--- video-platform/app/config/database.php
+++ video-platform/app/config/database.php
@@ -3,3 +3,16 @@
 // Pas de gegevens hieronder aan naar jouw eigen database
 // $pdo is daarna beschikbaar in al je models en controllers
 
+
+$host     = 'localhost';
+$dbname   = 'StreamHive';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+} catch (PDOException $e) {
+    die("Databaseverbinding mislukt: " . $e->getMessage());
+}
+?>
