a7ee0da344557629ad6d893ff72c38d9369f9c8d
RikJongejan <6044223@mborijnland.nl>
2026-05-13 16:30:35 +0200

    login regiter beter

--- video-platform/app/controllers/AuthController.php
+++ video-platform/app/controllers/AuthController.php
@@ -15,7 +15,7 @@ $userModel = new User($pdo);
 $error = '';
 $action = $_GET['action'] ?? '';
 
-//login Code
+
 // login code
 if ($action === 'login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = trim($_POST['email'] ?? '');
@@ -29,7 +29,7 @@ if ($action === 'login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['user_id']  = $user['id'];
             $_SESSION['username'] = $user['username'];
             $_SESSION['role']     = $user['role'];
-            header('Location: /video-platform/public/index.php');
+            header('Location: /github/streamhive/video-platform/views/index.php');
             exit;
         } else {
             $error = 'Invalid credentials';
@@ -40,4 +40,35 @@ if ($action === 'login' && $_SERVER['REQUEST_METHOD'] === 'POST') {
 // Altijd de view tonen als action=login (ook bij eerste bezoek en bij fouten)
     if ($action === 'login') {
         require_once __DIR__ . '/../../views/auth/login.php';
+}
+
+//register Code
+if ($action === 'register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email    =   trim($_POST['email'] ?? '');
+    $username =   trim($_POST['username'] ?? '');
+    $password =   $_POST['password'] ?? '';
+    $confirm  =   $_POST['confirm_password'] ?? '';
+
+    if (empty($email) || empty($username) || empty($password) || empty($confirm)) {
+        $error = 'Fill in all the fields.';
+    } elseif ($password !== $confirm) {
+        $error = 'Passwords dont match!';
+    } elseif (strlen($password) < 8) {
+        $error = 'password needs atleast 8 characters';
+    } else {
+        $result = $userModel->register($email, $username, $password);
+        if ($result) {
+            $user = $userModel->login($email, $password);
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+            header('Location: /github/streamhive/video-platform/views/index.php');
+            exit;
+        } else {
+            $error = 'Email or Username already in use.';
+        }
+    } 
+}
+if ($action === 'register') {
+    require_once __DIR__ . '/../../views/auth/register.php';
 }
--- video-platform/views/auth/register.php
+++ video-platform/views/auth/register.php
@@ -3,3 +3,40 @@
 // Toont het registratieformulier voor nieuwe gebruikers
 // Velden: e-mail, wachtwoord, wachtwoord bevestigen
 // Bij submit verwerkt AuthController de gegevens
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="card">
+        <?php if (!empty($error)): ?>
+        <div class="error"><?= htmlspecialchars($error) ?></div>
+        <?php endif; ?>
+
+         <form method="POST" action="/github/streamhive/video-platform/app/controllers/AuthController.php?action=register">
+            <label>E-mailadres</label>
+            <input type="email" name="email" placeholder="jouw@email.com" required>
+ 
+            <label>Gebruikersnaam</label>
+            <input type="text" name="username" placeholder="jouwusername" required>
+ 
+            <label>Wachtwoord</label>
+            <input type="password" name="password" placeholder="Minimaal 8 tekens" required>
+ 
+            <label>Wachtwoord bevestigen</label>
+            <input type="password" name="confirm_password" placeholder="••••••••" required>
+ 
+            <button type="submit">Account aanmaken</button>
+        </form>
+
+        <div class="link">
+            Al een account? <a href="/GitHub/StreamHive/video-platform/app/controllers/AuthController.php?action=login">Inloggen</a>
+        </div>
+
+    </div>
+</body>
+</html>
