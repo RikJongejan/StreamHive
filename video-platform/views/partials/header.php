16ed0401360db553b7b456fd4e44f4ba53e12e80
RikJongejan <6044223@mborijnland.nl>
2026-06-11 00:36:15 +0200

    html taal van nl naar en veranderd in header

--- video-platform/views/partials/header.php
+++ video-platform/views/partials/header.php
@@ -6,7 +6,7 @@ $pageTitle = $pageTitle ?? SITE_NAME;
 $hideNav   = $hideNav ?? false;
 ?>
 <!DOCTYPE html>
-<html lang="nl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
