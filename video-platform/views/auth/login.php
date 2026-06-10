c6d939d8d884cba612f6a1f76f4fc6396623a9d4
RikJongejan <6044223@mborijnland.nl>
2026-06-11 00:36:07 +0200

    loginpagina tekst van nederlands naar engels veranderd

--- video-platform/views/auth/login.php
+++ video-platform/views/auth/login.php
@@ -3,7 +3,7 @@
 // Toont het loginformulier met e-mail en wachtwoordveld
 // Geeft een foutmelding als de inlogpoging mislukt
 $error     = $error ?? '';
-$pageTitle = 'Inloggen';
+$pageTitle = 'Log In';
 $hideNav   = true;
 require VIEWS_PATH . '/partials/header.php';
 ?>
@@ -16,8 +16,8 @@ require VIEWS_PATH . '/partials/header.php';
                  onerror="this.onerror=null;this.src='<?= ASSETS_URL ?>/images/logo.svg'">
         </div>
 
-        <h1>Welkom terug</h1>
-        <p class="sub">Log in en duik weer in de korf</p>
+        <h1>Welcome back</h1>
+        <p class="sub">Sign in and dive back into the hive</p>
 
         <?php if (!empty($error)): ?>
             <div class="alert alert-error">
@@ -28,22 +28,22 @@ require VIEWS_PATH . '/partials/header.php';
 
         <form method="POST" action="<?= route('auth/login') ?>">
             <div class="form-group">
-                <label>E-mailadres</label>
-                <input class="input" type="email" name="email" placeholder="jij@email.com" required autofocus>
+                <label>Email address</label>
+                <input class="input" type="email" name="email" placeholder="you@email.com" required autofocus>
             </div>
 
             <div class="form-group">
-                <label>Wachtwoord</label>
+                <label>Password</label>
                 <input class="input" type="password" name="password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;" required>
             </div>
 
             <button class="btn btn-honey btn-block btn-lg" type="submit">
-                <i class="fa-solid fa-right-to-bracket"></i> Inloggen
+                <i class="fa-solid fa-right-to-bracket"></i> Log in
             </button>
         </form>
 
         <div class="link">
-            Nog geen account? <a href="<?= route('auth/register') ?>">Word lid van de Hive</a>
+            Don't have an account? <a href="<?= route('auth/register') ?>">Join the Hive</a>
         </div>
 
     </div>
